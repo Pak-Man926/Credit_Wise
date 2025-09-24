e0865dbacce1e271103b1779d8d696e5d0453ea8
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-09-24 22:47:59 +0300

    Bottom Navigation Bar Widget design

--- credit_wise_flutter/lib/app/components/widgets/bottom_navigation_widget.dart
+++ credit_wise_flutter/lib/app/components/widgets/bottom_navigation_widget.dart
@@ -2,17 +2,26 @@ import "package:flutter/material.dart";
 import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 class BottomNavigationWidget extends StatelessWidget {
-  final VoidCallback? onPressed;
-  final List<BottomNavigationBarItem> items;
   final int currentIndex;
+  final Function(int) onTap;
+  final Color? selectedItemColor;
+  final Color? unselectedItemColor;
+  final List<BottomNavigationBarItem>? items;
 
-  const BottomNavigationWidget(
-      {this.onPressed, required this.items, this.currentIndex = 0, super.key});
+  const BottomNavigationWidget({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+    this.selectedItemColor,
+    this.unselectedItemColor,
+    this.items,
+  });
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
-      onPressed: onPressed,
+      onTap: onTap,
+      currentIndex: currentIndex,
       items: [
         BottomNavigationBarItem(
             icon: Icon(PhosphorIcons.house()), label: "Home"),
