0090df6b83e2cc4a8036420fb5a874a8ffb6d4db
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-09-25 17:10:05 +0300

    Text field widget design

---
+++ credit_wise_flutter/lib/app/components/widgets/textfield_widget.dart
@@ -0,0 +1,39 @@
+import "package:flutter/material.dart";
+
+class TextFieldWidget extends StatelessWidget {
+
+  final String? hintText;
+  final TextEditingController? controller;
+  final bool obscureText;
+  final TextInputType? keyboardType;
+  final double? height;
+
+  const TextFieldWidget({
+    super.key,
+    this.hintText,
+    this.controller,
+    this.obscureText = false,
+    this.keyboardType,
+    this.height = 70.0,
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: height,
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        keyboardType: keyboardType,
+        decoration: InputDecoration(
+          hintText: hintText,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+        ),
+      ),
+    );
+  }
+}
