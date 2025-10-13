ddb685de471a02aa0cf624ea1d74438229ef0397
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-10-13 17:28:58 +0300

    Definition of constants

---
+++ credit_wise_flutter/lib/app/utils/constants.dart
@@ -0,0 +1,7 @@
+import "package:flutter/material.dart";
+
+final smallSpaceSize = SizedBox(height: 10);
+final mediumSpaceSize = SizedBox(height: 20);
+
+final App_Name = "Credit Wise";
+final App_Version = "1.0.0";
