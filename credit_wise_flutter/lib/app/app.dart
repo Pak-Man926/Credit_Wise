461170ea089fa636202b02cbe0f639da6111bd8d
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-10-13 17:32:22 +0300

    Resolving app concerns from the main file

--- credit_wise_flutter/lib/app/app.dart
+++ credit_wise_flutter/lib/app/app.dart
@@ -1,8 +1,22 @@
+import "package:credit_wise_flutter/app/theme/app_theme.dart";
+import "package:credit_wise_flutter/app/utils/constants.dart";
 import "package:flutter/material.dart";
+import "package:get/get.dart";
 
 class App extends StatelessWidget {
+  const App({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      top: false,
+      bottom: true,
+      child: GetMaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: App_Name,
+        initialRoute: AppPages.INITIAL,
+        getPages: AppPages.routes,
+        theme: appTheme,
+      ));
   }
 }
--- credit_wise_flutter/lib/main.dart
+++ credit_wise_flutter/lib/main.dart
@@ -1,9 +1,21 @@
 //import "package:credit_wise_flutter/app/routes.dart";
+import "package:credit_wise_flutter/app/modules/onboarding/views/getting_started_page.dart";
+import "package:credit_wise_flutter/app/theme/app_theme.dart";
 import "package:flutter/material.dart";
 //import "package:get/get.dart";
 import "package:credit_wise_flutter/app/app.dart";
 
-void main() 
-{
+void main() {
   runApp(App());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: appTheme,
+      home: const GettingStarted(),
+    );
+  }
 }
