f0896dfacb5796a162fb5dc32e1574c9680302bc
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-10-21 14:39:48 +0300

    Refactoring of the textbox widget to input field

---
+++ credit_wise_flutter/lib/app/data/widgets/input_field_widget.dart
@@ -0,0 +1,46 @@
+import "package:credit_wise_flutter/app/theme/app_colors.dart";
+import "package:credit_wise_flutter/app/theme/app_text_styles.dart";
+import "package:flutter/material.dart";
+
+class InputFieldWidget extends StatelessWidget {
+  final String? hintText;
+  final Color? inputBgColor;
+  final bool obscureText;
+  //final keyboardType;
+
+  const InputFieldWidget({
+    super.key,
+    required this.hintText,
+    this.inputBgColor = AppColors.inputBackground,
+    this.obscureText = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal:12, vertical: 12),
+      child: Container(
+        height: 60,
+        decoration: BoxDecoration(
+          color: inputBgColor,
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 2.0),
+          child: TextField(
+            obscureText: obscureText,
+            style: AppTextStyles.body,
+            decoration: InputDecoration(
+              hintText: hintText,
+              hintStyle: AppTextStyles.inputPlaceholder,
+              border: InputBorder.none,
+              focusedBorder: InputBorder.none,
+              enabledBorder: InputBorder.none,
+              contentPadding: const EdgeInsets.all(16),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
