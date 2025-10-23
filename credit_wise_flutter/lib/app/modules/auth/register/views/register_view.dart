ba806c2eefc214d99534870f784890b4903ee648
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-10-23 23:44:24 +0300

    Registration field input feilds definition

--- credit_wise_flutter/lib/app/modules/auth/register/views/register_view.dart
+++ credit_wise_flutter/lib/app/modules/auth/register/views/register_view.dart
@@ -1,3 +1,4 @@
+import 'package:credit_wise_flutter/app/data/widgets/input_field_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controllers/register_controller.dart';
@@ -8,8 +9,24 @@ class RegistrationView extends GetView<RegistrationController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Create Account')),
-      
+      appBar: AppBar(
+        title: const Text('Create Account'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          children: [
+            InputFieldWidget(hintText: "Enter your first name:"),
+            InputFieldWidget(hintText: "Enter your Last name:"),
+            InputFieldWidget(hintText: "Enter your  email address:"),
+            InputFieldWidget(hintText: "Enter your phone number:"),
+            InputFieldWidget(
+              hintText: "Enter your password:",
+              obscureText: true,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
