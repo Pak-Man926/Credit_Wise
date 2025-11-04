8110557dae49d9e379448929e306113ccbd4b38f
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-11-04 17:44:07 +0300

    Widget design start

---
+++ credit_wise_flutter/lib/app/data/widgets/home_feed_widget.dart
@@ -0,0 +1,12 @@
+import "package:flutter/material.dart";
+
+class HomeFeedWidget extends StatelessWidget {
+  const HomeFeedWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
