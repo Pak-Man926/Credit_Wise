a079ad98e820da53a161f2bd01662c311ffbc0bf
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-11-04 17:43:55 +0300

    Removal of place holders

--- credit_wise_flutter/lib/app/modules/home/views/home_view.dart
+++ credit_wise_flutter/lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,6 @@
+import 'package:credit_wise_flutter/app/theme/app_colors.dart';
+import 'package:credit_wise_flutter/app/theme/app_text_styles.dart';
+import 'package:credit_wise_flutter/app/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
@@ -8,7 +11,23 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     
+      backgroundColor: AppColors.background,
+     appBar: AppBar(
+      centerTitle: true,
+       title: Text("Home", style: AppTextStyles.headingXL),
+     ),
+     body: SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text("Welcome back, David", style: AppTextStyles.headingXL),
+            mediumSpaceSize,
+            
+          ],
+        ),
+      ),
+     )
     );
   }
 }
