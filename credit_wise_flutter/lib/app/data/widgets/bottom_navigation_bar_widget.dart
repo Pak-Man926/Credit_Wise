47c7ff8a5d8fd1929fdd999b7d2c3b74938ca297
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-11-04 12:34:40 +0300

    Bottom navigation bar completion

--- credit_wise_flutter/lib/app/data/widgets/bottom_navigation_bar_widget.dart
+++ credit_wise_flutter/lib/app/data/widgets/bottom_navigation_bar_widget.dart
@@ -1,6 +1,9 @@
+import "package:credit_wise_flutter/app/theme/app_colors.dart";
+import "package:credit_wise_flutter/app/theme/app_text_styles.dart";
 import "package:credit_wise_flutter/app/theme/app_theme.dart";
 import "package:flutter/material.dart";
 import "package:phosphor_flutter/phosphor_flutter.dart";
+
 
 class AppBottomNavigationBar extends StatelessWidget {
   final int currentIndex;
@@ -24,32 +27,36 @@ class AppBottomNavigationBar extends StatelessWidget {
       //type: BottomNavigationBarType.fixed,
       currentIndex: currentIndex,
       onTap: onTap,
-      selectedItemColor: appTheme.bottomNavigationBarTheme.selectedItemColor,
-      unselectedItemColor:
-          appTheme.bottomNavigationBarTheme.unselectedItemColor,
+      unselectedItemColor: AppColors.accent,
+      selectedItemColor: AppColors.primaryText,
+      selectedFontSize: 8,
+      unselectedFontSize: 8,
+      showUnselectedLabels: true,
+      selectedLabelStyle: AppTextStyles.bodySmall,
+      unselectedLabelStyle: AppTextStyles.bodySmall,
       items: [
         BottomNavigationBarItem(
             icon: Icon(
               PhosphorIcons.house(),
-              size: 32.0,
+              size: 24.0,
             ),
             label: "Home"),
         BottomNavigationBarItem(
             icon: Icon(
               PhosphorIcons.presentationChart(),
-              size: 32.0,
+              size: 24.0,
             ),
             label: "Credit Score"),
         BottomNavigationBarItem(
             icon: Icon(
               PhosphorIcons.star(),
-              size: 32.0,
+              size: 24.0,
             ),
             label: "Recommendations"),
         BottomNavigationBarItem(
             icon: Icon(
               PhosphorIcons.user(),
-              size: 32.0,
+              size: 24.0,
             ),
             label: "Profile"),
       ],
