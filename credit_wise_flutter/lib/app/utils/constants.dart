a9cc575b4d01733770a7351835b5bfb5d1f03cfd
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-11-11 18:24:24 +0300

    Creation of large space size constant

--- credit_wise_flutter/lib/app/utils/constants.dart
+++ credit_wise_flutter/lib/app/utils/constants.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 
 final smallSpaceSize = SizedBox(height: 10);
 final mediumSpaceSize = SizedBox(height: 20);
+final largeSpaceSize = SizedBox(height: 30);
 
 final App_Name = "Credit Wise";
 final App_Version = "1.0.0";
