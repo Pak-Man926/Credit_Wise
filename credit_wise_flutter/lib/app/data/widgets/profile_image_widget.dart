e833e377868a472d07f9446d243c3936fedab30a
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-11-11 18:23:28 +0300

    Profile image completion

--- credit_wise_flutter/lib/app/data/widgets/profile_image_widget.dart
+++ credit_wise_flutter/lib/app/data/widgets/profile_image_widget.dart
@@ -3,16 +3,16 @@ import "package:flutter/material.dart";
 class ProfileImageWidget extends StatelessWidget {
   const ProfileImageWidget({
     super.key,
-    this.profile_pic,});
+    required this.profilePic,
+  });
 
-  final Image? profile_pic;
+  final String profilePic;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: Image.asset("$profile_pic"),
-    ));
+    return CircleAvatar(
+      radius: 60,
+      backgroundImage: AssetImage(profilePic),
+    );
   }
 }
