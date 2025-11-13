ac18f58e33f0869642cd60d464d79bae624b88e1
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-11-13 23:57:59 +0300

    Loan offers view completion

--- credit_wise_flutter/lib/app/modules/loan_offers/views/loan_offer_view.dart
+++ credit_wise_flutter/lib/app/modules/loan_offers/views/loan_offer_view.dart
@@ -1,5 +1,8 @@
+import 'package:credit_wise_flutter/app/data/widgets/loan_offers_feed_widget.dart';
+import 'package:credit_wise_flutter/app/utils/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../../theme/app_text_styles.dart';
 import '../controllers/loan_offers_controller.dart';
 
 class LoanOffersView extends GetView<LoanOffersController> {
@@ -8,18 +11,46 @@ class LoanOffersView extends GetView<LoanOffersController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Personalized Loan Offers')),
-      body: Center(
-        child: Obx(() => ListView.builder(
-          itemCount: controller.offers.length,
-          itemBuilder: (context, index) {
-            final offer = controller.offers[index];
-            return ListTile(
-              title: Text(offer['bank']!),
-              subtitle: Text('Amount: ${offer['amount']}, Rate: ${offer['rate']}'),
-            );
-          },
-        )),
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Loan Offers', style: AppTextStyles.headingL),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text("Personalized Offers", style: AppTextStyles.headingXL),
+            mediumSpaceSize,
+            LoanOffersFeedWidget(
+              headlineText: "Sponsered",
+              bottomlineText: "Get a low rate on a home equity line of credit",
+              onPressed: () {},
+              image: "assets/loan_recommendations_1.png",
+              midlineText: "Home Equity Line of Credit",
+              buttonLabel: "View Offer",
+            ),
+            mediumSpaceSize,
+            LoanOffersFeedWidget(
+              headlineText: "Sponsered",
+              bottomlineText: "Get a low rate on a personal loan",
+              onPressed: () {},
+              image: "assets/loan_recommendations_2.png",
+              midlineText: "Personal Loan",
+              buttonLabel: "View Offer",
+            ),
+            mediumSpaceSize,
+            LoanOffersFeedWidget(
+              headlineText: "Sponsered",
+              bottomlineText: "Get a low rate on an auto loan",
+              onPressed: () {},
+              image: "assets/loan_recommendations_3.png",
+              midlineText: "Auto Loan",
+              buttonLabel: "View Offer",
+            ),
+          ],
+        ),
       ),
     );
   }
