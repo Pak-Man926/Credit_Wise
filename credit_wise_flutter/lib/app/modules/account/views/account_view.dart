d3c15ad2ff6034aaf9812a87310b69362bbf2834
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-11-14 16:14:59 +0300

    Account view design pending

--- credit_wise_flutter/lib/app/modules/account/views/account_view.dart
+++ credit_wise_flutter/lib/app/modules/account/views/account_view.dart
@@ -1,5 +1,7 @@
+import 'package:credit_wise_flutter/app/data/widgets/profile_image_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import '../../../theme/app_text_styles.dart';
 import '../controllers/account_controller.dart';
 
 class AccountView extends GetView<AccountController> {
@@ -8,19 +10,19 @@ class AccountView extends GetView<AccountController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Account & Settings')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Obx(() => Text('User Email: ${controller.userEmail.value}')),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: controller.logout,
-              child: const Text('Log Out'),
-            ),
-          ],
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Account', style: AppTextStyles.headingL),
         ),
+      body: Column(
+        children:[
+          Center(
+            child: Column(
+              children: [
+                ProfileImageWidget(profilePic: "assets/female_account_user.png"),
+              ]
+            ),),
+        ],
       ),
     );
   }
