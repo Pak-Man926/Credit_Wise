fdf17329a378e65152ae3372594f2c5470c7f532
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-11-17 16:56:46 +0300

    Account view design definition

--- credit_wise_flutter/lib/app/modules/account/views/account_view.dart
+++ credit_wise_flutter/lib/app/modules/account/views/account_view.dart
@@ -2,6 +2,7 @@ import 'package:credit_wise_flutter/app/data/widgets/profile_image_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../theme/app_text_styles.dart';
+import '../../../utils/constants.dart';
 import '../controllers/account_controller.dart';
 
 class AccountView extends GetView<AccountController> {
@@ -14,15 +15,26 @@ class AccountView extends GetView<AccountController> {
         centerTitle: true,
         title: const Text('Account', style: AppTextStyles.headingL),
         ),
-      body: Column(
-        children:[
-          Center(
-            child: Column(
-              children: [
-                ProfileImageWidget(profilePic: "assets/female_account_user.png"),
-              ]
-            ),),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:[
+            Center(
+              child: Column(
+                children: [
+                  ProfileImageWidget(profilePic: "assets/female_account_user.png"),
+                  smallSpaceSize,
+                  const Text("Jane Mwaura", style: AppTextStyles.headingL),
+                  smallSpaceSize,
+                  const Text("San Francisco, CA", style: AppTextStyles.bodySmall),
+        
+                ]
+              ),),
+            mediumSpaceSize,
+            const Text("Account & Settings", style: AppTextStyles.headingL),
+          ],
+        ),
       ),
     );
   }
