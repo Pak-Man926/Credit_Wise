b319c23eb961739b79d9d2f822673ca6cc3be2a6
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-11-20 16:37:28 +0300

    Account view design completion

--- credit_wise_flutter/lib/app/modules/account/views/account_view.dart
+++ credit_wise_flutter/lib/app/modules/account/views/account_view.dart
@@ -1,3 +1,4 @@
+import 'package:credit_wise_flutter/app/data/widgets/button_widget.dart';
 import 'package:credit_wise_flutter/app/data/widgets/profile_image_widget.dart';
 import 'package:credit_wise_flutter/app/data/widgets/profile_selection_widget.dart';
 import 'package:flutter/material.dart';
@@ -15,23 +16,22 @@ class AccountView extends GetView<AccountController> {
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Account', style: AppTextStyles.headingL),
-        ),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children:[
+          children: [
             Center(
-              child: Column(
-                children: [
-                  ProfileImageWidget(profilePic: "assets/female_account_user.png"),
-                  smallSpaceSize,
-                  const Text("Jane Mwaura", style: AppTextStyles.headingL),
-                  smallSpaceSize,
-                  const Text("San Francisco, CA", style: AppTextStyles.bodySmall),
-        
-                ]
-              ),),
+              child: Column(children: [
+                ProfileImageWidget(
+                    profilePic: "assets/female_account_user.png"),
+                smallSpaceSize,
+                const Text("Jane Mwaura", style: AppTextStyles.headingL),
+                smallSpaceSize,
+                const Text("San Francisco, CA", style: AppTextStyles.bodySmall),
+              ]),
+            ),
             mediumSpaceSize,
             const Text("Account & Settings", style: AppTextStyles.headingL),
             smallSpaceSize,
@@ -45,6 +45,33 @@ class AccountView extends GetView<AccountController> {
               leadingIcon: Icons.person_outline_outlined,
               accountSelection: "Personal",
             ),
+            mediumSpaceSize,
+            ProfileSelectionWidget(
+              leadingIcon: Icons.star_border_outlined,
+              accountSelection: "Preferences",
+            ),
+            mediumSpaceSize,
+            ProfileSelectionWidget(
+              leadingIcon: Icons.insert_link_outlined,
+              accountSelection: "Linked Accounts",
+            ),
+            mediumSpaceSize,
+            ProfileSelectionWidget(
+              accountSelection: "Support",
+              leadingIcon: Icons.contact_support_outlined,
+            ),
+            mediumSpaceSize,
+            ProfileSelectionWidget(
+              leadingIcon: Icons.security_outlined,
+              accountSelection: "Privacy & Security",
+            ),
+            mediumSpaceSize,
+            ProfileSelectionWidget(
+              accountSelection: "Notifications",
+              leadingIcon: Icons.notifications_outlined,
+            ),
+            largeSpaceSize,
+            CustomButton(text: "Sign Out"),
           ],
         ),
       ),
