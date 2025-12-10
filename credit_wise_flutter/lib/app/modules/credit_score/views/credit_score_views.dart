ddb739334e584dceda802697a3d897e7c5c81144
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-12-10 13:25:19 +0300

    Commenting on unused import

--- credit_wise_flutter/lib/app/modules/credit_score/views/credit_score_views.dart
+++ credit_wise_flutter/lib/app/modules/credit_score/views/credit_score_views.dart
@@ -1,6 +1,6 @@
 import 'package:credit_wise_flutter/app/data/widgets/profile_image_widget.dart';
 import 'package:credit_wise_flutter/app/utils/constants.dart';
-import 'package:fl_chart/fl_chart.dart';
+//import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../../theme/app_text_styles.dart';
