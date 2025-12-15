7beb8c5dae48bd561f64c22ef1d83d62a14e00ef
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-12-15 19:21:41 +0300

    Unnecessary api encryption

--- credit_wise_flutter/lib/app/data/services/network_service.dart
+++ credit_wise_flutter/lib/app/data/services/network_service.dart
@@ -1,18 +1,18 @@
-import "dart:async";
-import "package:credit_wise_flutter/app/data/services/config_service.dart";
-import "package:flutter/material.dart";
+// import "dart:async";
+// import "package:credit_wise_flutter/app/data/services/config_service.dart";
+// import "package:flutter/material.dart";
 
-class NetworkService {
-  final ConfigService _configService;
+// class NetworkService {
+//   final ConfigService _configService;
 
-  NetworkService(this._configService);
+//   NetworkService(this._configService);
 
-  Future<String> getApiBaseUrl() async {
-    // Ensure the config is loaded before accessing the API base URL
-    await _configService.loadConfig();
+//   Future<String> getApiBaseUrl() async {
+//     // Ensure the config is loaded before accessing the API base URL
+//     await _configService.loadConfig();
 
-    debugPrint("Protected API Base URL: ${_configService.apiBaseUrl}");
+//     debugPrint("Protected API Base URL: ${_configService.apiBaseUrl}");
     
-    return _configService.apiBaseUrl;
-  }
-}
+//     return _configService.apiBaseUrl;
+//   }
+// }
--- credit_wise_flutter/lib/app/utils/secrets.dart
+++ credit_wise_flutter/lib/app/utils/secrets.dart
@@ -1,9 +1,9 @@
-// lib/utils/secret_constants.dart
-import 'package:encrypt/encrypt.dart';
+// // lib/utils/secret_constants.dart
+// import 'package:encrypt/encrypt.dart';
 
-// ⚠️ In a real app, these would be derived, split, or obfuscated heavily.
-// KEY MUST be 32 bytes (256 bits) for AES-256
-final secretKey = Key.fromUtf8('thisisasecretkeyforflutterapp123'); 
+// // ⚠️ In a real app, these would be derived, split, or obfuscated heavily.
+// // KEY MUST be 32 bytes (256 bits) for AES-256
+// final secretKey = Key.fromUtf8('thisisasecretkeyforflutterapp123'); 
 
-// IV MUST be 16 bytes (128 bits)
-final secretIV = IV.fromUtf8('randomsalt123456');
+// // IV MUST be 16 bytes (128 bits)
+// final secretIV = IV.fromUtf8('randomsalt123456');
