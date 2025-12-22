70a0268e300904536e044c2f5c7abcfa02cce93f
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2025-12-22 22:11:16 +0300

    URL definition

--- credit_wise_flutter/lib/app/utils/constants.dart
+++ credit_wise_flutter/lib/app/utils/constants.dart
@@ -7,5 +7,7 @@ final largeSpaceSize = SizedBox(height: 30);
 final App_Name = "Credit Wise";
 final App_Version = "1.0.0";
 
-const baseUrl = "http://10.0.2.2:8080/";
+const emulatorBaseUrl = "http://10.0.2.2:8080/";
+
+const webBaseUrl = "http://localhost:8080/";
 
