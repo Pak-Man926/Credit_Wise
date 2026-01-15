28194ff038c3649b78876c00b87d9f5fc1920960
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-01-15 18:32:34 +0300

    Serverpod migration fix

---
+++ credit_wise_server/migrations/20260115134916142/migration.sql
@@ -0,0 +1,41 @@
+BEGIN;
+
+--
+-- ACTION DROP TABLE
+--
+DROP TABLE "auth_user" CASCADE;
+
+--
+-- ACTION CREATE TABLE
+--
+CREATE TABLE "auth_user" (
+    "id" bigserial PRIMARY KEY,
+    "firstName" text NOT NULL,
+    "secondName" text NOT NULL,
+    "lastName" text NOT NULL,
+    "email" text NOT NULL,
+    "phoneNumber" bigint NOT NULL,
+    "password" text NOT NULL,
+    "gender" text NOT NULL,
+    "createdAt" timestamp without time zone NOT NULL
+);
+
+
+--
+-- MIGRATION VERSION FOR credit_wise
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('credit_wise', '20260115134916142', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20260115134916142', "timestamp" = now();
+
+--
+-- MIGRATION VERSION FOR serverpod
+--
+INSERT INTO "serverpod_migrations" ("module", "version", "timestamp")
+    VALUES ('serverpod', '20251208110333922-v3-0-0', now())
+    ON CONFLICT ("module")
+    DO UPDATE SET "version" = '20251208110333922-v3-0-0', "timestamp" = now();
+
+
+COMMIT;
