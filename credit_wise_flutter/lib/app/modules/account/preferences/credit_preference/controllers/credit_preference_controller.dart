73459f0ff86e01cf50263574a0e947f5d15163e4
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-01-18 23:54:19 +0300

    Credit Preference page completion

--- credit_wise_flutter/lib/app/modules/account/preferences/credit_preference/controllers/credit_preference_controller.dart
+++ credit_wise_flutter/lib/app/modules/account/preferences/credit_preference/controllers/credit_preference_controller.dart
@@ -1,9 +1,43 @@
 import 'package:get/get.dart';
 
 class CreditPreferenceController extends GetxController {
-  //TODO: Implement CreditPreferenceController
+  var selectedUsage = "".obs;
+  var selectedLateRepayments = "".obs;
+  var selectedActiveLoans = "".obs;
 
-  final count = 0.obs;
+  final usageItems = [
+    "I dont use credit",
+    "Very little (0-20%)",
+    "Some (20-40%)",
+    "About half (40-60%)",
+    "Most (60-80%)",
+    "Almost all (80-100%)",
+    "Not sure",
+  ];
+
+  final lateRepaymentItems = [
+    "Never",
+    "1-2 times",
+    "3-5 times",
+    "More than 5 times",
+    "Not sure",
+  ];
+
+  final activeLoansItems = ["None", "1", "2-3", "4-5", "6+"];
+
+  void setUsage(String? value) {
+    selectedUsage.value = value ?? "";
+  }
+
+  void setLateRepayments(String? value) {
+    selectedLateRepayments.value = value ?? "";
+  }
+
+  void setActiveLoans(String? value) {
+    selectedActiveLoans.value = value ?? "";
+  }
+  
+
   @override
   void onInit() {
     super.onInit();
@@ -18,6 +52,4 @@ class CreditPreferenceController extends GetxController {
   void onClose() {
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
--- credit_wise_flutter/lib/app/modules/account/preferences/credit_preference/views/credit_preference_view.dart
+++ credit_wise_flutter/lib/app/modules/account/preferences/credit_preference/views/credit_preference_view.dart
@@ -1,3 +1,7 @@
+import 'package:credit_wise_flutter/app/data/widgets/button_widget.dart';
+import 'package:credit_wise_flutter/app/data/widgets/drop_down_widget.dart';
+import 'package:credit_wise_flutter/app/theme/app_text_styles.dart';
+import 'package:credit_wise_flutter/app/utils/constants.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -10,13 +14,58 @@ class CreditPreferenceView extends GetView<CreditPreferenceController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('CreditPreferenceView'),
+        title: const Text('Credit Preferences', style: AppTextStyles.headingL),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'CreditPreferenceView is working',
-          style: TextStyle(fontSize: 20),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: .start,
+          children: [
+            const Text(
+              "If you use credit(credit card/overdraft), how much do you typically use?)",
+              style: AppTextStyles.body,
+            ),
+            smallSpaceSize,
+            Obx(
+              () => DropDownWidget(
+                hintText: "Select usage",
+                items: controller.usageItems,
+                value: controller.selectedUsage.value,
+                onChanged: (val) => controller.setUsage(val),
+              ),
+            ),
+            mediumSpaceSize,
+            const Text(
+              "In the 2 years, how often have you been late on loan repayments?",
+              style: AppTextStyles.body,
+            ),
+            smallSpaceSize,
+            Obx(
+              () => DropDownWidget(
+                hintText: "Select frequency",
+                items: controller.lateRepaymentItems,
+                value: controller.selectedLateRepayments.value,
+                onChanged: (val) => controller.setLateRepayments(val),
+              ),
+            ),
+            mediumSpaceSize,
+            const Text(
+              "How many active loans or credit accounts do you have?",
+              style: AppTextStyles.body,
+            ),
+            smallSpaceSize,
+            Obx(
+              () => DropDownWidget(
+                hintText: "Select number of accounts",
+                items: controller.activeLoansItems,
+                value: controller.selectedActiveLoans.value,
+                onChanged: (val) => controller.setActiveLoans(val),
+              ),
+            ),
+            largeSpaceSize,
+            CustomButton.primary(text: "Submit", onPressed: () {}),
+          ],
         ),
       ),
     );
