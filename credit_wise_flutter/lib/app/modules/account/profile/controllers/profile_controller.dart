f141e6c067777396fb44186957c96f1276a2a910
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-01-22 23:53:49 +0300

    Profile controller definition

--- credit_wise_flutter/lib/app/modules/account/profile/controllers/profile_controller.dart
+++ credit_wise_flutter/lib/app/modules/account/profile/controllers/profile_controller.dart
@@ -1,26 +1,96 @@
+import 'package:credit_wise_client/credit_wise_client.dart';
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:logger/logger.dart';
+import 'package:serverpod_flutter/serverpod_flutter.dart';
 
 class ProfileController extends GetxController {
+  late final Client client;
+
+  var logger = Logger();
+
   var selection = "".obs;
 
-  void selectedOption(String? value)
-  {
-    selection.value = value ?? "";
-  }
+  final ageController = TextEditingController();
+  final dependantsController = TextEditingController();
+  final incomeController = TextEditingController();
+  final debtRepaymentsController = TextEditingController();
+  final contributorsIncomeController = TextEditingController();
 
   @override
   void onInit() {
     super.onInit();
+
+    client = Client('http://localhost:8080/')
+      ..connectivityMonitor = FlutterConnectivityMonitor();
+
+    logger.d("Profile Controller Initialized");
   }
 
   @override
   void onReady() {
     super.onReady();
+
+    logger.d("Profile UI is now visible to the user");
   }
 
   @override
   void onClose() {
+    ageController.dispose();
+    dependantsController.dispose();
+    incomeController.dispose();
+    debtRepaymentsController.dispose();
+    contributorsIncomeController.dispose();
     super.onClose();
   }
 
+  void selectedOption(String? value) {
+    selection.value = value ?? "";
+  }
+
+  Future<void> registerProfileData() async {
+    logger.d("Registering Profile Data...");
+
+    logger.i(
+      "Age: '${ageController.text}' \n No. of Dependants: '${dependantsController.text}' \n Monthly Income: '${incomeController.text}' \n Debt Repayments: '${debtRepaymentsController.text}' \n Contributors Income: '${contributorsIncomeController.text}'",
+    );
+
+    //final debtRatio = (debtRepaymentsController.text) / (incomeController.text);
+
+    if (ageController.text.isEmpty ||
+        dependantsController.text.isEmpty ||
+        incomeController.text.isEmpty ||
+        debtRepaymentsController.text.isEmpty ||
+        (selection.value == "Yes" &&
+            contributorsIncomeController.text.isEmpty)) {
+      Get.snackbar("Error", "Please fill in all fields");
+
+      logger.w("Profile Data is Incomplete");
+
+      return;
+
+    } else {
+      logger.d("All Profile Data fields are filled. Proceeding...");
+      try{
+        await client.profile.createProfileData(
+          2,
+          int.parse(ageController.text.trim()),
+          int.parse(dependantsController.text.trim()),
+          double.parse(incomeController.text.trim()),
+          double.parse(debtRepaymentsController.text.trim()) /
+              double.parse(incomeController.text.trim()),
+          selection.value == "Yes"
+              ? double.parse(contributorsIncomeController.text.trim())
+              : 0.0,
+        );
+
+        logger.i("Profile Data Registered Successfully");
+
+        Get.snackbar("Success", "Profile Data Registered Successfully");
+      } catch (e) {
+        logger.e("Error registering profile data: $e");
+        Get.snackbar("Error", "Failed to register profile data");
+      }
+    }
+  }
 }
